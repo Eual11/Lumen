42c1af7e7b3e845c34acb89706f34a5d79db7d8d
Eual11 <uchihaeual11@gmail.com>
2025-04-15 11:45:02 +0300

    testing the new pipeline

--- main.py
+++ main.py
@@ -2,33 +2,37 @@ import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtWidgets import QFileDialog
 from PySide6 import QtCore
-from app import LumenMainWindow,test
+from app import LumenMainWindow
+from app.widgets import Renderer
 from app.widgets.DicomViewer import DicomViewer
 
 from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+
+from core import DicomLoader
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.ui = LumenMainWindow.Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.loader = DicomLoader.DicomLoader()
 
-        self.view = DicomViewer()
-        self.view2 = DicomViewer()
+
+        self.view = DicomViewer(self.loader.get_output_port())
+        self.renderer = Renderer.Renderer()
 
         self.ui.vtkContainer.addWidget(self.view)
-        self.ui.vtkContainer.addWidget(self.view2)
+        self.ui.vtkContainer.addWidget(self.renderer)
 
 
         #NOTE: testing buttons and loading
 
 
         self.ui.loadBtn.clicked.connect(lambda: self.load(0))
-        self.ui.renderBtn.clicked.connect(lambda: self.load(1))
 
     def closeEvent(self, event) -> None:
         self.view.cleanup()
-        self.view2.cleanup()
+        self.renderer.cleanup()
         return super().closeEvent(event)
 
     @QtCore.Slot()
@@ -36,20 +40,9 @@ class MainWindow(QMainWindow):
         dir = QFileDialog.getExistingDirectory(None, "Select Dicom Folder")
 
         if(dir and n==0):
-            self.view.loadImage(dir)
-        if(dir and n==1):
-            self.view2.loadImage(dir)
-
-
-
-
-
-
-
-        
-
-
-
+            self.loader.load_imge(dir)
+            self.view.updateSource(self.loader.get_output_port())
+    
 
 def main():
     app = QApplication(sys.argv)

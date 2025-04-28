8050d17ae45535d2543f9e5e5ce00a38922655d0
Eual11 <uchihaeual11@gmail.com>
2025-04-28 15:49:31 +0300

    segementation algorithms

---
+++ core/SegementationAlgorithms.py
@@ -0,0 +1,35 @@
+from Segment import Segement
+import SimpleITK as sitk
+
+from vtk import vtkImageData
+
+"""Abstractions for SITK Segementation Algorithms to work on Segements """
+
+class ThresholdFilter:
+    lower_threshold:int
+    upper_threshold:int
+    inside_value:int
+    outside_value:int
+
+    _filter: sitk.BinaryThresholdImageFilter
+
+    def __init__(self, lower_threshold:int =0, upper_threshold = 255, inside_value:int=1, outside_value:int =0) -> None:
+       self.lower_threshold = lower_threshold
+       self.upper_threshold = upper_threshold
+       self.inside_value = inside_value 
+       self.outside_value = outside_value 
+       self._filter = sitk.BinaryThresholdImageFilter()
+
+       
+    def Execute(self, image:vtkImageData,segement:Segement, op ="add"):
+
+       #TODO: a function to convert vtkImgedata to  
+
+       self._filter.SetLowerThreshold(self.lower_threshold) 
+       self._filter.SetUpperThreshold(self.upper_threshold) 
+       self._filter.SetInsideValue(self.inside_value) 
+       self._filter.SetOutsideValue(self.outside_value)
+
+
+        
+
